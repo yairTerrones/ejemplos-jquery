8ba21ced92320fc9b0751af94378e47f2d8caad9
Daniel Torres <youkiddingd@gmail.com>
2017-03-08 12:58:31 -0600

    Ejemplos de jQuery.

---
+++ holajquery.js
@@ -0,0 +1,15 @@
+//$(document).ready(saluda)
+
+//function saluda() {
+//alert($('h1').text())
+//}
+
+$(function() {
+    //alert($('h1').text())
+
+    //Modificar le DOM.
+    //Crear un parrafo nuevo en un objeto jQuery.
+    $nuevo = $('<p>Parrafo <b>nuevo</b></p>')
+
+    $('h2').after($nuevo)
+})
